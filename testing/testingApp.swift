938e70c3c044d85db36c122c0bc7fabd7b854695
Thorsten Ball <mrnugget@gmail.com>
2023-07-01 08:23:43 +0200

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Thorsten Ball on 01.07.23.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
